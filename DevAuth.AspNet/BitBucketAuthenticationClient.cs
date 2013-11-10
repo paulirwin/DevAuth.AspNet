0fa8fa8602df1e0c51f7ce6e54e71adfd52a0ffd
Paul Irwin <paulirwin@gmail.com>
2013-11-10 17:52:21 -0500

    First code commit

---
+++ DevAuth.AspNet/BitBucketAuthenticationClient.cs
@@ -0,0 +1,96 @@
+﻿using DotNetOpenAuth.AspNet;
+using DotNetOpenAuth.AspNet.Clients;
+using DotNetOpenAuth.Messaging;
+using DotNetOpenAuth.OAuth;
+using DotNetOpenAuth.OAuth.ChannelElements;
+using DotNetOpenAuth.OAuth.Messages;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevAuth.AspNet
+{
+    public class BitBucketAuthenticationClient : OAuthClient
+    {
+        public static readonly ServiceProviderDescription BitBucketServiceDescription;
+
+        static BitBucketAuthenticationClient()
+        {
+            ServiceProviderDescription spd = new ServiceProviderDescription();
+
+            spd.RequestTokenEndpoint = new MessageReceivingEndpoint("https://bitbucket.org/api/1.0/oauth/request_token", HttpDeliveryMethods.PostRequest | HttpDeliveryMethods.AuthorizationHeaderRequest);
+            spd.UserAuthorizationEndpoint = new MessageReceivingEndpoint("https://bitbucket.org/api/1.0/oauth/authenticate", HttpDeliveryMethods.GetRequest | HttpDeliveryMethods.AuthorizationHeaderRequest);
+            spd.AccessTokenEndpoint = new MessageReceivingEndpoint("https://bitbucket.org/api/1.0/oauth/access_token", HttpDeliveryMethods.PostRequest | HttpDeliveryMethods.AuthorizationHeaderRequest);
+            spd.TamperProtectionElements = new[] { new HmacSha1SigningBindingElement() };
+
+            BitBucketServiceDescription = spd;
+        }
+
+        private readonly string _consumerKey;
+        private readonly string _consumerSecret;
+
+        public BitBucketAuthenticationClient(string consumerKey, string consumerSecret)
+            : this(consumerKey, consumerSecret, new AuthenticationOnlyCookieOAuthTokenManager())
+        {
+        }
+
+        public BitBucketAuthenticationClient(string consumerKey, string consumerSecret, IOAuthTokenManager tokenManager)
+            : base("BitBucket", BitBucketServiceDescription, new SimpleConsumerTokenManager(consumerKey, consumerSecret, tokenManager))
+        {
+            _consumerKey = consumerKey;
+            _consumerSecret = consumerSecret;
+        }
+
+        protected override AuthenticationResult VerifyAuthenticationCore(AuthorizedTokenResponse response)
+        {
+            var msg = response as ITokenSecretContainingMessage;
+
+            if (msg == null)
+                return AuthenticationResult.Failed;
+
+            string oauthToken = msg.Token;
+            string oauthTokenSecret = msg.TokenSecret;
+
+            var authenticator = OAuth1Authenticator.ForProtectedResource(_consumerKey, _consumerSecret, oauthToken, oauthTokenSecret);
+
+            Uri uri = new Uri("https://bitbucket.org/api/1.0/user");
+
+            var request = new RestRequest(Method.GET);
+            request.Resource = uri.AbsoluteUri;
+
+            var client = new RestClient();
+            client.Authenticator = authenticator;
+
+            var apiresponse = client.Execute(request);
+
+            if (apiresponse.StatusCode != HttpStatusCode.OK)
+                return AuthenticationResult.Failed;
+
+            string userName = null;
+
+            try
+            {
+                var json = JObject.Parse(apiresponse.Content);
+
+                userName = json["user"]["username"].ToString();
+            }
+            catch
+            {
+                return AuthenticationResult.Failed;
+            }
+
+            Dictionary<string, string> strs = new Dictionary<string, string>()
+            {
+                { "accesstoken", response.AccessToken }
+            };
+
+            return new AuthenticationResult(true, base.ProviderName, userName, userName, strs);
+        }
+    }
+}
---
+++ DevAuth.AspNet/GitHubAuthenticationClient.cs
@@ -0,0 +1,104 @@
+﻿using DotNetOpenAuth.AspNet;
+using DotNetOpenAuth.AspNet.Clients;
+using DotNetOpenAuth.Messaging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevAuth.AspNet
+{
+    public class GitHubAuthenticationClient : OAuth2Client
+    {
+        public const string AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+
+        public const string TokenEndpoint = "https://github.com/login/oauth/access_token";
+
+        public const string UsersEndpoint = "https://api.github.com/users";
+
+        private readonly string _clientID;
+
+        private readonly string _clientSecret;
+
+        public GitHubAuthenticationClient(string clientID, string clientSecret)
+            : base("GitHub")
+        {
+            _clientID = clientID;
+            _clientSecret = clientSecret;
+        }
+
+        protected override Uri GetServiceLoginUrl(Uri returnUrl)
+        {
+            UriBuilder uriBuilder = new UriBuilder(AuthorizationEndpoint);
+            uriBuilder.AppendQueryArgument("client_id", _clientID);
+            uriBuilder.AppendQueryArgument("redirect_uri", returnUrl.AbsoluteUri.Replace("localhost", "127.0.0.1"));
+            //uriBuilder.AppendQueryArgument("scope", "user");
+
+            return uriBuilder.Uri;
+        }
+
+        protected override IDictionary<string, string> GetUserData(string accessToken)
+        {
+            var client = new RestClient();
+
+            var builder = new UriBuilder(UsersEndpoint);
+
+            builder.AppendQueryArgument("access_token", accessToken);
+
+            var request = new RestRequest(builder.Uri, Method.GET);
+
+            var response = client.Execute(request);
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                return new Dictionary<string, string>();
+
+            var json = JObject.Parse(response.Content);
+
+            var strs = new Dictionary<string, string>();
+
+            strs["id"] = json["id"].ToString();
+            strs["username"] = json["login"].ToString();
+            strs["name"] = json["name"].ToString();
+
+            return strs;
+        }
+
+        protected override string QueryAccessToken(Uri returnUrl, string authorizationCode)
+        {
+            try
+            {
+                var client = new RestClient();
+
+                var request = new RestRequest(TokenEndpoint, Method.POST);
+
+                request.AddParameter("client_id", _clientID);
+                request.AddParameter("redirect_uri", returnUrl.AbsoluteUri);
+                request.AddParameter("client_secret", _clientSecret);
+                request.AddParameter("code", authorizationCode);
+
+                request.AddHeader("accept", "application/json");
+
+                var response = client.Execute(request);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var oAuth2AccessTokenDatum = JsonConvert.DeserializeObject<OAuth2AccessTokenData>(response.Content);
+                    if (oAuth2AccessTokenDatum != null)
+                    {
+                        return oAuth2AccessTokenDatum.AccessToken;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            return null;
+        }
+    }
+}
